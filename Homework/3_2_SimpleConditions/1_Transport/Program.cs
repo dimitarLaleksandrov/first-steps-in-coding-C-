e358ed8a1e5f6b706f04d3076de620c03ade6892
dimitarLaleksandrov <dimitar.l.aleksandrov@gmail.com>
2021-09-17 15:46:54 +0300

    Exercise 1 complete

---
+++ Homework/3_2_SimpleConditions/1_Transport/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1_Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kmTravled = int.Parse(Console.ReadLine());
+            string dayOrNightTariff = Console.ReadLine();
+            if (kmTravled < 20)
+            {
+                if (dayOrNightTariff == "day")
+                {
+                    Console.WriteLine((kmTravled * 0.79) + 0.70);
+                }
+                else
+                {
+                    Console.WriteLine((kmTravled * 0.90) + 0.70);
+                }
+            }
+            else if (kmTravled >= 20 && kmTravled < 100)
+            {
+                Console.WriteLine(kmTravled * 0.09);
+            }
+            else
+            {
+                Console.WriteLine(kmTravled * 0.06);
+            } 
+        }
+    }
+}
