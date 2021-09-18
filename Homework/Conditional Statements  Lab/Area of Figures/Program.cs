61d40bf82cc8c3a924df53bef2e96a55cb56a413
dimitarLaleksandrov <dimitar.l.aleksandrov@gmail.com>
2021-09-18 12:55:57 +0300

    Conditional Statements - Lab

---
+++ Homework/Conditional Statements  Lab/Area of Figures/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area = 0; 
+            if (figure == "square")
+            {
+                double num = double.Parse(Console.ReadLine());
+                area = num * num;
+                Console.WriteLine(area);
+            }
+            else if(figure == "rectangle")
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine());
+                area = num1 * num2;
+                Console.WriteLine(area);
+            }
+            else if(figure == "circle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = Math.PI * (side * side);
+                Console.WriteLine(area);
+            }
+            else if(figure == "triangle")
+            {
+                double length = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                area = (length * height) / 2;
+                Console.WriteLine(area);
+
+            }
+
+
+        }
+    }
+}
