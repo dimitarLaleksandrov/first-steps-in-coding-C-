ebe425d8b21c436f4f06b5294472dc35d15e4627
dimitarLaleksandrov <dimitar.l.aleksandrov@gmail.com>
2021-09-27 19:43:18 +0300

    Home worke

---
+++ Homework/7.0 Conditional Statements Advanced - Lab/07.Working Hours/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            if (hours >= 10 && hours <= 18)
+            {
+                switch (day)
+                {
+                    case "Monday":
+                    case "Tuesday":
+                    case "Wednesday":
+                    case "Thursday":
+                    case "Friday":
+                    case "Saturday":
+                        Console.WriteLine("open");
+                        break;
+                    case "Sunday":
+                        Console.WriteLine("closed");
+                        break;
+                    default:
+                        Console.WriteLine("closed");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
+
---
+++ Homework/7.0 Conditional Statements Advanced - Lab/10. Invalid Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10._Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if(num < 100 || num > 200)
+            {
+                if (num != 0)
+                {
+                    Console.WriteLine("invalid");
+                }
+            }
+        }
+    }
+}
