5c3f6ef4be81d9319069ef0789ea107af81312f6
Kir4o <kiko.toshev@gmail.com>
2021-09-28 21:59:16 +0300

    Upload Chapter_3_2

---
+++ Homework/Chapter_3_2_Simple_checks/pipes in the pool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pipes_in_the_pool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pool Liters :");
+            int poolVolume = int.Parse(Console.ReadLine());
+            Console.Write("Pipe one flow :");
+            int flowPipeOne = int.Parse(Console.ReadLine());
+            Console.Write("Pipe two flow :");
+            int flowPipeTwo = int.Parse(Console.ReadLine());
+            Console.Write("Hours :");
+            double hoursWorkerAbsent = double.Parse(Console.ReadLine());
+            double waterFull = Math.Floor((flowPipeOne * hoursWorkerAbsent) + (flowPipeTwo * hoursWorkerAbsent));
+            if(poolVolume >= waterFull)
+            {
+                double waterPercent = Math.Floor((waterFull / poolVolume) * 100);
+                double pipeOnePercent = Math.Floor(((flowPipeOne * hoursWorkerAbsent) / waterFull) *100);
+                double pipeTwoPercent = Math.Floor(((flowPipeTwo * hoursWorkerAbsent) / waterFull) * 100);
+                Console.WriteLine($"The pool is {waterPercent}% full. Pipe1:{pipeOnePercent}%.Pipe2:{pipeTwoPercent}%.");
+            }
+            else
+            {
+                Console.WriteLine($"For {hoursWorkerAbsent} hours the pool overflows with {waterFull - poolVolume} liters.");
+            }
+        }
+    }
+}
---
+++ Homework/Chapter_3_2_Simple_checks/the sleepy cat Tom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace the_sleepy_cat_Tom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int restDays = int.Parse(Console.ReadLine());
+            int hostAtWork = (365 - restDays) * 63;
+            int hostInRests = restDays * 127;
+            var totalPlayMinuts = hostAtWork + hostInRests;
+            var difarent = Math.Abs(totalPlayMinuts - 30000);
+            var hours = difarent / 60;
+            var min = difarent % 60;
+            if(totalPlayMinuts > 30000)
+            {
+                Console.WriteLine("Tom will run away!");
+                Console.WriteLine($"{hours} hours {min} minutes more to play!");
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{hours} hours and {min} minutes less for play");
+            }
+        }
+    }
+}
