ab1ae8551a255ca09fa4221cec5f5425048e5832
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2022-03-24 17:01:00 +0200

    Exams

---
+++ Homework/Fundamentals whit C#/33. Exam Preparation/Problem 1 - Secret Chat/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_1___Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine(); 
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] commandArgs = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+                string commandType = commandArgs[0];
+                if (commandType == "InsertSpace")
+                {
+                    int intToInsert = int.Parse(commandArgs[1]);
+                    message = message.Insert(intToInsert, " ");
+                }
+                else if (commandType == "Reverse")
+                {
+                    string reverseString = commandArgs[1];
+                    if (message.Contains(reverseString))
+                    {
+                        int wordIndex = message.IndexOf(reverseString);
+                        message = message.Remove(wordIndex, reverseString.Length);
+                        message += string.Join("", reverseString.Reverse());
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+                    
+                }
+                else if (commandType == "ChangeAll")
+                {
+                    string substring = commandArgs[1];
+                    string replaistmentText = commandArgs[2];
+                    message = message.Replace(substring, replaistmentText);
+                }
+                Console.WriteLine(message);
+            }
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
---
+++ Homework/Fundamentals whit C#/33. Exam Preparation/Problem 2 - Mirror words/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Problem_2___Mirror_words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(@|#)([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1");
+            string words = Console.ReadLine();
+            MatchCollection matches = pattern.Matches(words);
+            List<string[]> result = new List<string[]>();
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+            }
+            foreach (Match match in matches)
+            {
+                string firstWord = match.Groups[2].Value;
+                string secondWord = match.Groups[3].Value;
+                string reversedSecondWord = string.Join("", secondWord.Reverse());
+                if (firstWord == reversedSecondWord)
+                {
+                    result.Add(new string[] { firstWord, secondWord });
+                }
+            }
+            if (result.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                string[] messages = result.Select(word => $"{word[0]} <=> {word[1]}").ToArray();
+                Console.WriteLine($"The mirror words are: ");
+                Console.WriteLine(string.Join(", ", messages));
+            }
+        }
+    }
+}
