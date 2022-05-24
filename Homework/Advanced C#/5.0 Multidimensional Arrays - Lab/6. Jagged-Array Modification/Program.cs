624fd453b22d0d11e7bd5d5311bc394e45f966e6
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2022-05-24 11:52:38 +0300

    jagged array

---
+++ Homework/Advanced C#/5.0 Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[rowsCount][];
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                jagged[row] = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            }
+            while (true)
+            {
+                string[] cmd = Console.ReadLine().Split(' ');
+                if (cmd[0] == "END")
+                {
+                    break;
+                }
+                else if (cmd[0] == "Add" || cmd[0] == "Subtract")
+                {
+                    int row = int.Parse(cmd[1]);
+                    int col = int.Parse(cmd[2]);
+                    int val = int.Parse(cmd[3]);
+                    if (cmd[0] == "Subtract")
+                    {
+                        val = -val;
+                    }
+                    if (row >= 0 && row < jagged.Length && col >= 0 && col < jagged[row].Length)
+                    {
+                        jagged[row][col] += val;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }                
+                } 
+            }
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                Console.WriteLine(string.Join(" ", jagged[row]));
+            } 
+        }
+    }
+}
---
+++ Homework/Advanced C#/5.0 Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace _7._Pascal_Triangle
+{
+    internal class PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger[][] triangal = new BigInteger[n][]; 
+            for (int row = 0; row < n; row++)
+            {
+                triangal[row] = new BigInteger[row + 1];
+                triangal[row][0] = 1;
+                for (int col = 1; col < row; col++)
+                {
+                    triangal[row][col] = triangal[row - 1][col - 1] + triangal[row - 1][col];
+                }
+                triangal[row][row] = 1;
+            }
+            for (int row = 0; row < triangal.Length; row++)
+            {
+                Console.WriteLine(String.Join(" ", triangal[row]));
+            }
+        }
+    }
+}
