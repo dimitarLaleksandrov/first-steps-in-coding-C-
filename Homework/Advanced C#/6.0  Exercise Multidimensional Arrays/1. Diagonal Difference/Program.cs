dee23ce3d445fc55688200a693cdbb62cfb59dce
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2022-05-24 17:37:54 +0300

    Multidimensional Arrays - Exercise

---
+++ Homework/Advanced C#/6.0  Exercise Multidimensional Arrays/1. Diagonal Difference/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    internal class DiagonalDifference
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int leftCrosSum = 0;
+            int rithCrosSum = 0;
+            FillMatrix(matrix);
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        leftCrosSum += matrix[row, col];    
+                    }
+                    if (row + col == n - 1)
+                    {
+                        rithCrosSum += matrix[row, col];
+                    }
+                }
+            }
+            int difference = Math.Abs(leftCrosSum - rithCrosSum);
+            Console.WriteLine(difference);
+        }
+        private static void FillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+        }
+    }
+}
---
+++ Homework/Advanced C#/6.0  Exercise Multidimensional Arrays/2. Squares in Matrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int rows = int.Parse(input.Split()[0]);
+            int cols = int.Parse(input.Split()[1]);
+            string[,] matrix = new string[rows, cols];
+            FillMatrix(matrix);
+            int matrixCounter2X2 = 0;
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        matrixCounter2X2++;
+                    }
+                }
+            }
+            Console.WriteLine(matrixCounter2X2);
+        }
+        private static void FillMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] rowData = Console.ReadLine().Split(" ");
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+
+    }
+}
