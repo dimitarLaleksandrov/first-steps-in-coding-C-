51c0e4e81226196a4144afdfd95fbd9d0aceab9b
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2022-05-27 13:58:42 +0300

    Sets and Dictionaries Advanced - Exercise

---
+++ Homework/Advanced C#/8.0 Exercise Sets and Dictionaries Advanced/06. Wardrobe/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Wardrobe
+{
+    internal class Wardrobe
+    {
+        static void Main(string[] args)
+        {
+            int loopNums = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, int>> clothes = new Dictionary<string, Dictionary<string, int>>();
+            for (int i = 1; i <= loopNums; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ");
+                string collor = input[0];
+                string[] cloth = input[1].Split(",");
+                if (!clothes.ContainsKey(collor))
+                {
+                    clothes.Add(collor, new Dictionary<string, int>());
+                    foreach (var c in cloth)
+                    {
+                        if (!clothes[collor].ContainsKey(c))
+                        {
+                            clothes[collor].Add(c, 1);
+                        }
+                        else
+                        {
+                            clothes[collor][c]++;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var c in cloth)
+                    {
+                        if (!clothes[collor].ContainsKey(c))
+                        {
+                            clothes[collor].Add(c, 1);
+                        }
+                        else
+                        {
+                            clothes[collor][c]++;
+                        }
+                    }
+                }
+            }
+            string[] dressToFound = Console.ReadLine().Split(" ");
+            string clotheColor = dressToFound[0];
+            string clothe = dressToFound[1];
+            foreach (var collor in clothes)
+            {
+                Console.WriteLine($"{collor.Key} clothes:");
+                foreach (var cloth in collor.Value)
+                {
+                    if (cloth.Key.Contains(clothe) && collor.Key.Contains(clotheColor))
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+                    }
+                }
+            }
+            
+        }
+    }
+}
