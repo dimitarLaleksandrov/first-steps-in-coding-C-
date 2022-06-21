3c80355dacae8b6fe797eda843b4b7fb041f7381
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2022-06-21 19:36:50 +0300

    Exam Preparation

---
+++ Homework/Advanced C#/21.0 Exam Preparation/Drones/1 Scheduling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1_Scheduling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasksStakInput = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var threadsQueuesInput = new List<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int valueOfTheTask = int.Parse(Console.ReadLine());
+            var kiledTask = 0;
+            while (true)
+            {
+                var threadValue = threadsQueuesInput[0];
+                var taskValue = tasksStakInput.Peek();
+                if (taskValue == valueOfTheTask)
+                {
+                    kiledTask = threadValue;
+                    break;
+                }
+                if (threadValue > taskValue || threadValue < taskValue || threadValue == taskValue)
+                {
+                    threadsQueuesInput.Remove(threadValue);
+                }
+                if (threadValue >= taskValue)
+                {
+                    tasksStakInput.Pop();
+                } 
+            }
+            Console.WriteLine($"Thread with value {kiledTask} killed task {valueOfTheTask}");
+            Console.WriteLine(string.Join(" ", threadsQueuesInput));
+        }
+    }
+}
---
+++ Homework/Advanced C#/21.0 Exam Preparation/Drones/2. Garden/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Garden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var matrixRow = matrixSize[0];
+            var matrixCol = matrixSize[1];
+            int[,] garden = new int[matrixRow, matrixCol];
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                int[] flowerCoordinates = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                var flowerRow = flowerCoordinates[0];
+                var flowerCol = flowerCoordinates[1];
+                if(flowerRow < 0 || flowerCol < 0 || flowerRow >= matrixRow || flowerCol >= matrixCol)
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    continue;
+                }
+                else
+                {
+                    var flower = new int[flowerRow, flowerCol];
+                    for (int row = 0; row < matrixRow; row++)
+                    {
+                        for (int col = 0; col < matrixCol; col++)
+                        {
+                            if (col == flowerCol)
+                            {
+                                if (garden[row, col] == 0)
+                                {
+                                    garden[row, col] = 1;
+                                }
+                                else if(garden[row, col] != garden[flowerRow, flowerCol])
+                                {
+                                    garden[row, col] += 1;
+                                }
+                                else
+                                {
+                                    garden[row, col] += 1;
+                                }
+                            }
+                            else if (row == flowerRow)
+                            {
+                                if (garden[row, col] == 0)
+                                {
+                                    garden[row, col] = 1;
+                                }
+                                else if(garden[row, col] != garden[flowerRow, flowerCol])
+                                {
+                                    garden[row, col] += 1;
+                                }
+                                else
+                                {
+                                    garden[row, col] += 1;
+                                }
+                            }
+                        }  
+                    }
+                }
+            }
+            PrintTheMatrix(garden);
+        }
+        public static void PrintTheMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (col > 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
