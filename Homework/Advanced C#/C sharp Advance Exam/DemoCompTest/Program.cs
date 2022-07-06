aa15675c5b201eb7739e97bb4ddb7ec742bbfc2d
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2022-07-06 15:05:59 +0300

    Inheritance - Lab

---
+++ Homework/Advanced C#/C sharp Advance Exam/DemoCompTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DemoCompTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10000000;
+            Stopwatch w = Stopwatch.StartNew();
+            Linear(n);
+            Console.WriteLine(w.ElapsedMilliseconds);
+        }
+
+        private static long Linear(int n)
+        {
+            long counter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                counter++;
+            }
+            return counter;
+        }
+    }
+}
---
+++ Homework/C# OOP/StartUp/CustomRandomList/RandomList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+
+        public RandomList(Random rnd)
+        {
+            this.Rnd = rnd;
+        }
+
+        public Random Rnd { get => rnd; set => rnd = value; }
+
+        public string RemoveRandomElement()
+        {
+            string str = string.Empty;
+            int index = Rnd.Next(0, this.Count);
+            str = this[index];
+            this.RemoveAt(index);
+            return str;
+        }
+
+        private void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
