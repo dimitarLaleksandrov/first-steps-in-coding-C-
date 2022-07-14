8fc01f21145880da8300bc17abcd4264b509fd69
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2022-07-14 13:28:34 +0300

    Polymorphism

---
+++ Homework/C# OOP/9.0 Polymorphism/Animals/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favoriteFood;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public string FavoriteFood 
+        { 
+            get { return favouriteFood; }
+            private set { favouriteFood = value; }
+        }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favorite food is {this.FavoriteFood}";
+        }
+    }
+}
