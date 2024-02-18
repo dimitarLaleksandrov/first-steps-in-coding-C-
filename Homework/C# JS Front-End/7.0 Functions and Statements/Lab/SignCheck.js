de9659ce873591d32540c4313f95003749383702
Dimitar Aleksandrov <dimitar.l.aleksandrov@gmail.com>
2024-02-18 20:41:08 +0200

    all new stuff

---
+++ Homework/C# JS Front-End/7.0 Functions and Statements/Lab/SignCheck.js
@@ -0,0 +1,17 @@
+function signCheck(a, b, c) {
+  let counter = 0;
+  if (a < 0) {
+    counter++;
+  }
+  if (b < 0) {
+    counter++;
+  }
+  if (c < 0) {
+    counter++;
+  }
+  if (counter % 2 == 0) {
+    console.log("Positive");
+  } else {
+    console.log("Negative");
+  }
+}
---
+++ Homework/C# JS Front-End/9.0 Objects and Classes/Lab/PersonInfo.js
@@ -0,0 +1,8 @@
+function createObject(firstName, lastName, age) {
+  return { firstName, lastName, age};
+  /* let object = {};
+  object.firstName = firstName;
+  object.lastName = lastName;
+  object.age = age;
+  return object; */
+}
